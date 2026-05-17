8c25362d5cce843134f07f555e0d4f3fd8361e85
Prasannaravamsi <prasannarao.leo@gmail.com>
2026-05-17 09:09:55 +0530

    export the java script function in helper file and import in test file

--- utils/LoginPageHelper.js
+++ utils/LoginPageHelper.js
@@ -1,7 +1,8 @@
 const {Page,expect}=require("@playwright/test")
 
-export async function openLoginPage(page) {
+async function openLoginPage(page) {
     await page.goto("/login")
    //await expect(page.locator("h1.text-xl").filter({ hasText: "Sign in to EventHub" })).toBeVisible()
    await expect(page.locator('h1:has-text("Sign in to EventHub")')).toBeVisible()
 }
+module.exports={openLoginPage}
